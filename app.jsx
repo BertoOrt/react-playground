df0d297b007f7c76bdd35af25e65e9bf91cfb165
Roberto Ortega <berto.ort@gmail.com>
2015-10-28 17:41:44 -0600

    react dom

--- app.jsx
+++ app.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
-import Hello from './components/app.jsx';
+// import Hello from './components/app.jsx';
+import ReactDOM from 'react-dom';
 
-React.render(<Hello/>, document.getElementById('main'));
+ReactDOM.render(
+	<h3>
+    Hello World
+  </h3>,
+  document.getElementById('main')
+);
+
+// import React from 'react';
+// import Hello from './components/app.jsx';
+//
+// React.render(<Hello/>, document.getElementById('main'));
--- components/app.jsx
+++ components/app.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default React.createClass({
-  render () {
-		return (
-			<h3>
-        Hello World
-      </h3>
-		);
-	}
-})
+export default ReactDOM.render(
+	<h3>
+    Hello World
+  </h3>
+)
+
+// export default React.createClass({
+//   render () {
+// 		return (
+// 			<h3>
+//         Hello World
+//       </h3>
+// 		);
+// 	}
+// })
